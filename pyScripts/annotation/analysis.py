588dfa4e3dd3e1ed0f61bb40cead77fbe39adc9e
bacor <basseman@gmail.com>
2014-10-21 01:24:39 +0200

    extracted annotations statistics

--- pyScripts/annotation/analysis.py
+++ pyScripts/annotation/analysis.py
@@ -4,5 +4,26 @@ import numpy as np
 import json
 
 questions = pickle.load(open('annotated_questions_final.p', 'rb'))
-pprint(questions[1])
-print 'boe'
+analysis = []
+for q in questions:
+
+	if len(q['mc_entities']) > 0:
+		avg_mc_entity_count = sum(map(len, q['mc_entities'])) / float(len(q['mc_entities']))
+	else:
+		avg_mc_entity_count = 0
+
+	a = {
+		'QID': q['QID'],
+
+		# Ratio of represented mc answers
+		'mc_representation': float(len(q['mc_entities'])) / len(q['mc']),
+
+		# Number of question entities
+		'q_entity_count': len(q['q_entities']),
+
+		# Average number of mc answer entities
+		'avg_mc_entity_count': avg_mc_entity_count
+	}
+	analysis.append(a)
+
+pickle.dump(analysis, open('analysis.p', 'w'))
